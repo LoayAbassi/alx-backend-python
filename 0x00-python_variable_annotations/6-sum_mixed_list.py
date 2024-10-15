242c7a4524b5286cdbd08d179e373479a2de14e0
LoayAbassi <abassi.loay@gmail.com>
2024-10-15 22:47:21 +0100

    concentration gone

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -7,7 +7,7 @@ returns their sum as a float.
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_ls: List[Union[int, str]])->float:
+def sum_mixed_list(mxd_ls: List[Union[int, float]]) -> float:
     """
     returns the sum of elements
     """
