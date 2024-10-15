7fafd978e0035b0012289d59ea9edbb1c7392ba7
LoayAbassi <abassi.loay@gmail.com>
2024-10-15 22:04:37 +0100

    ignoring some cached and test files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+constains the copncat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings together."""
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""contains a floor function"""
+
+
+def floor(n: float) -> int:
+    """returns the largest integer 
+    less than or equal to n"""
+    return int(n) if n >= 0 else int(n)-1
