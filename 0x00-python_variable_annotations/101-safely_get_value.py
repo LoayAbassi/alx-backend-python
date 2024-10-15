5171a5f6ed905eef87be17a5adc5f99519fdb721
LoayAbassi <abassi.loay@gmail.com>
2024-10-16 00:06:39 +0100

    pycodestyle is crazy

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,11 @@ from typing import Mapping, Any, Union, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None]
+        = None) -> Union[Any, T]:
     """adding annotations is harder than coding :) """
     if key in dct:
         return dct[key]
