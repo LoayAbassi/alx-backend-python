ff6c9b9c8185c407bd3db336f280fe8dfdda84f7
LoayAbassi <abassi.loay@gmail.com>
2024-10-16 13:26:25 +0100

    having a headache :(

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+"""contains a wait_n function"""
+
 import asyncio
+wait_random = __import__("0-basic_async_syntax").wait_random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
+async def wait_n(n: int, max_delay: int) -> list:
+    """returns a sorted list of n delays"""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    done = []
+    # awaiting all tasks
+    for completed in asyncio.as_completed(tasks) :
+        res = await completed
+        done.append(res)
+
+    return done
--- 0x01-python_async_function/README.md
+++ 0x01-python_async_function/README.md
@@ -6,4 +6,8 @@
     # called coroutine too :) 
     async def afunction(): 
         # do something
+
+asyncio.gather(...) // waits for all awaited tasks to finish 
+asyncio.create_task(...) // creates a task from a coroutine
+
         
