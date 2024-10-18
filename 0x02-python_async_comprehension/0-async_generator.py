c481d6081f9781260e1a745d94ecb19e93f08ae0
LoayAbassi <abassi.loay@gmail.com>
2024-10-18 22:38:35 +0100

    annotation changed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async generator."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """
     Generate a yielder.
     """
