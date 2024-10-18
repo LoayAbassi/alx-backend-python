933f168e873e2023f64be0263227c76f0d3fc6aa
LoayAbassi <abassi.loay@gmail.com>
2024-10-18 22:33:55 +0100

    now

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,12 +8,10 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     Generate a yielder.
-    
     Returns:
         AsyncGenerator[float, None, None]
-
     Yields:
-        Iterator[AsyncGenerator[float, None, None]]
+        Iterator[AsyncGenerator[float, None]]
     """
     for i in range(10):
         await asyncio.sleep(1)
