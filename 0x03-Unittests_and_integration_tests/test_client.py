84444c4781d0fb6ab541090db1a0952fd67c4700
LoayAbassi <abassi.loay@gmail.com>
2024-11-13 00:35:49 +0100

    pushing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""contains code"""
+"""some code"""
 from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,3 +18,11 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org)
         client.org
         mock.called_with_once(client.ORG_URL.format(org=org))
+
+    def test_public_repos_url(self):
+        "does something too"
+        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mocked_object:
+            mocked_object.return_value = {"repos_url": "salut_bb"}
+            haja = GithubOrgClient("staghferallah")
+            natija = haja._public_repos_url
+            self.assertEqual(natija, mocked_object.return_value["repos_url"])
